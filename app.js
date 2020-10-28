bca3e71fbf38879d01520016866c66be6a46563f
Nick Rinaldi <39104732+nickrinaldi88@users.noreply.github.com>
2020-10-28 15:16:26 -0400

    10/28 files

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("test");
+const list_node = document.createElement("li");
+const task = document.createTextNode("This is a task")
+list_node.appendChild(task)
+
+function createListElem() {
+    document.body.appendChild(list_node);
+    list_node.set
+}
+
+createListElem();
+
+let greeting = "say Hi";
+if (true) {
+    let greeting = "say Hello instead";
+    console.log(greeting); // "say Hello instead"
+}
+
+console.log(greeting);
+
+
+// "say Hello instead"
+
+
+// 1. When our button object is clicked(), I need to generate a ul element
+// 1(cont). containing the text inside our input box
+
+// Establish variable to hold input object
+// Learn about the differences in variable declarations
+// - https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/#:~:text=var%20declarations%20are%20globally%20scoped%20or%20function%20scoped%20while%20let,be%20updated%20nor%20re%2Ddeclared.
+
+// Learn how to dynmically place elements when button pressed
