047ff1039b466a62d44c06c945b107d4b8b7df93
Nick Rinaldi <39104732+nickrinaldi88@users.noreply.github.com>
2020-11-04 20:12:31 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
-const ul_node = document.createElement("ul");
-document.body.appendChild(ul_node);
+const ul_node = document.createElement("ul")
+const list_div = document.createElement('div');
+// list_div.appendChild(ul_node);
+
+
+// const add_btn = document.getElementById('add-btn');
+// add_btn.addEventListener("keypress", function(event){
+//     if (event.keyCode === 13){
+//         document.getElementById('add-btn').click();
+//     }
+// });
 
 function createListElem() {
-    const del_btn = document.createElement('button');
-    del_btn.setAttribute('id', 'del-btn')
-    del_btn.setAttribute('onclick', delete_task())
-    // document.body.appendChild(list_node);
-    // when button clicked element below placed onto page
-    // grab input content
+    const delete_btn = document.createElement('button');
+    delete_btn.setAttribute('id', 'del-btn');
+    delete_btn.onclick = function(){
+        this.parentElement.remove()
+    };
+    // li_item.addEventListener('click', 'remove')
+    // delete_btn.innerText = 'delete';
+
     const box_value = document.getElementById('input-box').value;
-    // create tex node of that value
+
     const task = document.createTextNode(box_value);
-    // create ul node
+
     const li_node = document.createElement('li');
-    // add  text to the ul
+
+    li_node.setAttribute('id', 'li-task');
+
+    // appending text to li element
     li_node.appendChild(task)
-    li_node.appendChild(del_btn)
-    // add ul to list
+    // appending button to li element *
+    li_node.appendChild(delete_btn);
+    // appending li element containing text and btn to ul element
     ul_node.appendChild(li_node)
-    // add list to body
-    // document.body.appendChild(list_node);
-    // console.log(box_value);
+    // appending ul element to div
+    list_div.appendChild(ul_node); 
+    document.body.appendChild(list_div);
+
+    // console.log(main_div);
+    // const main_div = document.getElementsByClassName("input-container");
     
+
 }
 
 function delete_task(){
-    const obj = document.querySelector('li');
-    obj.remove(); 1
+    const li_item = document.querySelector("li");
+    li_item.addEventListener('click', 'remove')
+
 }
 
-createListElem();
+// https://www.codexworld.com/how-to/trigger-button-click-on-enter-key-press-javascript/
 
-
-
-// Todo:
-// -Generate button in each ul element to delete that element
-// -Center our list below the todo list
-// -resize 
+// - Add new item on enter
+// - center our ul better
+// - Figure out why text in li is not displaying horizontally
+// - configure our drop down to sort list by completed, uncompleted, and all
+// - display another button for completed in the li element, and add a strikethrough to the li text
+// - style and color
+// - add fade-out effect for li element when deleted
